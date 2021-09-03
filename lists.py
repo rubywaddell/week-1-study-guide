b47d940368fbb0398048a8ec1ddeebdbcc330171
Ruby Waddell <rubywaddell@gmail.com>
2021-09-03 13:01:09 -0700

    Write reverse_list function using list splicing to return the original list backwards

--- lists.py
+++ lists.py
@@ -261,29 +261,27 @@ def join_strings_with_comma(words):
         joined_words += words[-1]
         return joined_words
 
-# def reverse_list(items):
-#     """Return the input list, reversed.
-#     **Do not use** the python function `reversed()` or the method
-#     `list.reverse()`.
+def reverse_list(items):
+    """Return the input list, reversed.
+    **Do not use** the python function `reversed()` or the method
+    `list.reverse()`.
     
-#     For example::
+    For example::
     
-#         >>> reverse_list([1, 2, 3])
-#         [3, 2, 1]
-#         >>> reverse_list(["cookies", "love", "I"])
-#         ['I', 'love', 'cookies']
+        >>> reverse_list([1, 2, 3])
+        [3, 2, 1]
+        >>> reverse_list(["cookies", "love", "I"])
+        ['I', 'love', 'cookies']
     
-#     You should do this without changing the original list::
+    You should do this without changing the original list::
     
-#         >>> orig = ["apple", "berry", "cherry"]
-#         >>> reverse_list(orig)
-#         ['cherry', 'berry', 'apple']
-#         >>> orig
-#         ['apple', 'berry', 'cherry']
-#     """
-
-#     return []
-
+        >>> orig = ["apple", "berry", "cherry"]
+        >>> reverse_list(orig)
+        ['cherry', 'berry', 'apple']
+        >>> orig
+        ['apple', 'berry', 'cherry']
+    """
+    return items[::-1]
 
 # def reverse_list_in_place(items):
 #     """Reverse the input list `in place`.
